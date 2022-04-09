81361617cc9c883263c7147a2ed552e082e16b2d
steven-rj <stevenrjfiles@gmail.com>
2022-04-08 21:27:56 -0400

    changed nav links, link to other site

--- server.py
+++ server.py
@@ -16,14 +16,6 @@ def html_page(page_name):
     return render_template(page_name)
 
 
-def write_to_file(data):
-    with open("database.txt", mode="a") as database:
-        email = data["email"]
-        subject = data["subject"]
-        message = data["message"]
-        file = database.write(f"\n{email},{subject},{message}")
-
-
 def write_to_csv(data):
     with open("database.csv", newline="", mode="a") as database2:
         email = data["email"]
